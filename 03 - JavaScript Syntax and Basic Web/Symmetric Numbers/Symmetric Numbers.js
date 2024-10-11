dcccd297c30f5c65e7aa0f0303f3cf47d9eec219
Nasko-FSD <atanasnw@gmail.com>
2024-10-11 16:55:00 +0300

    Symmetric Numbers

---
+++ 03 - JavaScript Syntax and Basic Web/Symmetric Numbers/Symmetric Numbers.js
@@ -0,0 +1,10 @@
+function symmetricNumbers([n]) {
+    n = Number(n);
+
+    for (let i = 1; i <= n; i++){
+        let numberAsString = "" + i;
+        if (numberAsString == [...numberAsString].reverse().join("")) {
+            console.log(i);
+        }
+    }
+}
