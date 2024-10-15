ab125512db8f394892a0fe9905d57c075344b7fe
Nasko-FSD <atanasnw@gmail.com>
2024-10-15 16:37:25 +0300

    Multiply Two Numbers

---
+++ 04 - JavaScript Syntax and Basic Web - Exercises/Multiply Two Numbers/Multiply Two Numbers Second Solve.js
@@ -0,0 +1,5 @@
+function multiplyTwoNumbersSecondSolve(arr) {
+    let numbers = arr.map(Number);
+    let multiply = numbers[0] * numbers[1];
+    console.log(multiply);
+}
---
+++ 04 - JavaScript Syntax and Basic Web - Exercises/Multiply Two Numbers/Multiply Two Numbers.js
@@ -0,0 +1,6 @@
+function multiplyTwoNumbers(arr) {
+    let firstNumber = Number(arr[0]);
+    let secondNumber = Number(arr[1]);
+    let multiply = firstNumber * secondNumber;
+    console.log(multiply);
+}
