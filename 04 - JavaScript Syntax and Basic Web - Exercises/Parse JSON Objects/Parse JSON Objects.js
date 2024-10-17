c2819576b1783a2c332660df6de34b592bec21c0
Nasko-FSD <atanasnw@gmail.com>
2024-10-17 16:49:43 +0300

    Parse JSON Objects

---
+++ 04 - JavaScript Syntax and Basic Web - Exercises/Parse JSON Objects/Parse JSON Objects.js
@@ -0,0 +1,10 @@
+function parseJSONObjects(args) {
+
+    for (let input of args) {
+        let obj = JSON.parse(input);
+
+        console.log(`Name: ${obj.name}`);
+        console.log(`Age: ${obj.age}`);
+        console.log(`Date: ${obj.date}`);
+    }
+}
