dc1cf250ab7e65922e5be860f80421a106d52e4f
Nasko-FSD <atanasnw@gmail.com>
2025-01-22 12:05:25 +0200

    C# ASP.NET Core MVC

---
+++ 14 - C# ASP.NET Core MVC Overview - Lab/Numbers from 1-50/Numbers from 1-50/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Numbers_from_1_50.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
