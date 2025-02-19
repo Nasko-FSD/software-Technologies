3f93d023743945eebd8ed29fa19ec0b9edff134f
Nasko-FSD <atanasnw@gmail.com>
2025-02-19 22:56:08 +0200

    C#

---
+++ 19 - Exam Prep 2/C#/IMDB/Models/IMDBDbContext.cs
@@ -0,0 +1,25 @@
+﻿namespace IMDB.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+
+    public class IMDBDbContext : DbContext
+    {
+        public DbSet<Film> Films { get; set; }
+
+        public IMDBDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=imdb_asp;user=root;");
+        }
+
+        internal object Where(Func<object, bool> value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 19 - Exam Prep 2/C#/IMDB/Startup.cs
@@ -0,0 +1,39 @@
+﻿namespace IMDB
+{
+    using Models;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using IMDB.Models;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => options.EnableEndpointRouting = false); // ✅ Fix added
+            services.AddDbContext<IMDBDbContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc();
+        }
+    }
+}
