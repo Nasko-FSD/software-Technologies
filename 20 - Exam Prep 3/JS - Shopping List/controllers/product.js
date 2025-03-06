9a42fc365eb8cc2970978e0ceab59542d2820103
Nasko-FSD <atanasnw@gmail.com>
2025-03-07 00:47:04 +0200

    JS - Shopping List

---
+++ 20 - Exam Prep 3/JS - Shopping List/controllers/product.js
@@ -0,0 +1,71 @@
+const Product = require('../models').Product;
+
+module.exports = {
+	index: (req, res) => {
+
+		let allProducts = Product.findAll().then( allProducts => {
+
+		res.render("product/index", {"entries": allProducts})
+		});
+	},
+
+	createGet: (req, res) => {
+
+		res.render("product/create");
+
+	},
+
+	createPost: (req, res) => {
+
+		let body = req.body;
+
+		Product.create(body).then(() => {
+			res.redirect("/");
+		})
+
+	},
+
+	editGet: (req, res) => {
+
+		let id = req.params.id;
+
+		Product.findById(id).then(product => {
+
+			res.render("product/edit", product.dataValues);
+		})
+
+
+	},
+
+	editPost: (req, res) => {
+
+		let id = req.params.id;
+		let body = req.body;
+
+		Product.findById(id).then(product => {
+			product.updateAttributes(body).then(() => {
+				res.redirect("/");
+			})
+		})
+	},
+
+	deleteGet: (req,res) => {
+
+		let id = req.params.id;
+
+		Product.findById(id).then(product => {
+			res.render("product/delete", product.dataValues);
+		})
+	},
+
+	deletePost: (req,res) => {
+
+		let id = req.params.id;
+
+		Product.findById(id).then(product => {
+		 	product.destroy().then(() => {
+				res.redirect("/");
+			})
+		})
+	}
+};
